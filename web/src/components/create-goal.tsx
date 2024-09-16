a4be35c3070d7af36196cc786aab0475af1ba974
ryandvl <ryandvlssc9914@gmail.com>
2024-09-16 00:02:52 -0300

    Aula 03

--- web/src/components/create-goal.tsx
+++ web/src/components/create-goal.tsx
@@ -1,28 +1,75 @@
-import {
-  DialogContent,
-  DialogTitle,
-  DialogClose,
-  DialogDescription,
-} from './ui/dialog'
+import { X } from 'lucide-react'
+import { Button } from './ui/button'
 import {
   RadioGroup,
-  RadioGroupItem,
   RadioGroupIndicator,
+  RadioGroupItem,
 } from './ui/radio-group'
-import { XIcon } from 'lucide-react'
-import { Button } from './ui/button'
+import {
+  DialogClose,
+  DialogContent,
+  DialogDescription,
+  DialogTitle,
+} from './ui/dialog'
 import { Input } from './ui/input'
 import { Label } from './ui/label'
+import { z } from 'zod'
+import { Controller, useForm } from 'react-hook-form'
+import { zodResolver } from '@hookform/resolvers/zod'
+import { createGoal } from '../http/create-goal'
+import { toast } from 'sonner'
+import { useQueryClient } from '@tanstack/react-query'
+
+const createGoalSchema = z.object({
+  title: z.string().min(1, 'Informe a atividade que deseja praticar'),
+  desiredWeeklyFrequency: z.coerce.number().min(1).max(7),
+})
+
+type CreateGoalSchema = z.infer<typeof createGoalSchema>
 
 export function CreateGoal() {
+  const queryClient = useQueryClient()
+
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+    control,
+    reset,
+  } = useForm<CreateGoalSchema>({
+    resolver: zodResolver(createGoalSchema),
+  })
+
+  async function handleCreateGoal({
+    title,
+    desiredWeeklyFrequency,
+  }: CreateGoalSchema) {
+    try {
+      await createGoal({
+        title,
+        desiredWeeklyFrequency,
+      })
+
+      reset()
+
+      queryClient.invalidateQueries({ queryKey: ['pending-goals'] })
+      queryClient.invalidateQueries({ queryKey: ['summary'] })
+
+      toast.success('Meta criada com sucesso!')
+    } catch {
+      toast.error('Erro ao criar a meta, tente novamente!')
+    }
+  }
+
   return (
     <DialogContent>
       <div className="flex flex-col gap-6 h-full">
-        <div className="flex flex-col gap-3">
+        <div className="space-y-3">
           <div className="flex items-center justify-between">
             <DialogTitle>Cadastrar meta</DialogTitle>
+
             <DialogClose>
-              <XIcon size={20} className="text-zinc-600" />
+              <X className="size-5 text-zinc-600" />
             </DialogClose>
           </div>
 
@@ -32,79 +79,71 @@ export function CreateGoal() {
           </DialogDescription>
         </div>
 
-        <form action="" className="flex-1 flex flex-col justify-between">
-          <div className="flex flex-col gap-6">
+        <form
+          onSubmit={handleSubmit(handleCreateGoal)}
+          className="flex-1 flex flex-col justify-between"
+        >
+          <div className="space-y-6">
             <div className="flex flex-col gap-2">
               <Label htmlFor="title">Qual a atividade?</Label>
+
               <Input
                 id="title"
                 autoFocus
                 placeholder="Praticar exercícios, meditar, etc..."
+                {...register('title')}
               />
+
+              {errors.title && (
+                <p className="text-sm text-red-400">{errors.title.message}</p>
+              )}
             </div>
+
             <div className="flex flex-col gap-2">
-              <Label htmlFor="title">Quantas vezes na semana?</Label>
-              <RadioGroup>
-                <RadioGroupItem value="1">
-                  <RadioGroupIndicator />
-                  <span className="text-zinc-300 text-sm font-medium leading-none">
-                    1x na semana
-                  </span>
-                  <span className="text-lg leading-none">🥱</span>
-                </RadioGroupItem>
-                <RadioGroupItem value="2">
-                  <RadioGroupIndicator />
-                  <span className="text-zinc-300 text-sm font-medium leading-none">
-                    2x na semana
-                  </span>
-                  <span className="text-lg leading-none">🙂</span>
-                </RadioGroupItem>
-                <RadioGroupItem value="3">
-                  <RadioGroupIndicator />
-                  <span className="text-zinc-300 text-sm font-medium leading-none">
-                    3x na semana
-                  </span>
-                  <span className="text-lg leading-none">😎</span>
-                </RadioGroupItem>
-                <RadioGroupItem value="4">
-                  <RadioGroupIndicator />
-                  <span className="text-zinc-300 text-sm font-medium leading-none">
-                    4x na semana
-                  </span>
-                  <span className="text-lg leading-none">😜</span>
-                </RadioGroupItem>
-                <RadioGroupItem value="5">
-                  <RadioGroupIndicator />
-                  <span className="text-zinc-300 text-sm font-medium leading-none">
-                    5x na semana
-                  </span>
-                  <span className="text-lg leading-none">🤨</span>
-                </RadioGroupItem>
-                <RadioGroupItem value="6">
-                  <RadioGroupIndicator />
-                  <span className="text-zinc-300 text-sm font-medium leading-none">
-                    6x na semana
-                  </span>
-                  <span className="text-lg leading-none">🤯</span>
-                </RadioGroupItem>
-                <RadioGroupItem value="all">
-                  <RadioGroupIndicator />
-                  <span className="text-zinc-300 text-sm font-medium leading-none">
-                    Todos dias da semana
-                  </span>
-                  <span className="text-lg leading-none">🔥</span>
-                </RadioGroupItem>
-              </RadioGroup>
+              <Label htmlFor="desiredWeeklyFrequency">
+                Quantas vezes na semana?
+              </Label>
+
+              <Controller
+                control={control}
+                name="desiredWeeklyFrequency"
+                defaultValue={5}
+                render={({ field }) => {
+                  return (
+                    <RadioGroup
+                      value={String(field.value)}
+                      onValueChange={field.onChange}
+                    >
+                      {Array.from({ length: 7 }).map((_, i) => {
+                        const frequency = String(i + 1)
+
+                        return (
+                          <RadioGroupItem key={String(i)} value={frequency}>
+                            <RadioGroupIndicator />
+                            <span className="text-zinc-300 text-sm font-medium leading-none">
+                              {frequency}x na semana
+                            </span>
+                            <span className="text-lg leading-none">🥱</span>
+                          </RadioGroupItem>
+                        )
+                      })}
+                    </RadioGroup>
+                  )
+                }}
+              />
             </div>
           </div>
 
-          <div className="flex items-center gap-3">
+          <div className="flex items-center gap-3 mt-auto">
             <DialogClose asChild>
-              <Button className="flex-1" variant="secondary">
+              <Button variant="secondary" className="flex-1">
                 Fechar
               </Button>
             </DialogClose>
-            <Button className="flex-1">Salvar</Button>
+
+            <Button type="submit" className="flex-1">
+              Salvar
+            </Button>
           </div>
         </form>
       </div>
--- web/src/components/empty-goals.tsx
+++ web/src/components/empty-goals.tsx
@@ -1,27 +1,31 @@
-import inOrbitLogo from '../assets/in-orbit-logo.svg'
+import { Plus } from 'lucide-react'
+import { DialogTrigger } from '@radix-ui/react-dialog'
+
+import logo from '../assets/in-orbit-logo.svg'
 import rocketLaunchIllustration from '../assets/rocket-launch-illustration.svg'
-
-import { PlusIcon } from 'lucide-react'
-
-import { Button } from '../components/ui/button'
-import { DialogTrigger } from '../components/ui/dialog'
+import { Button } from './ui/button'
 
 export function EmptyGoals() {
   return (
-    <div className="h-screen flex flex-col items-center justify-center gap-8">
-      <img src={inOrbitLogo} alt="in.orbit" />
-      <img src={rocketLaunchIllustration} alt="in.orbit" />
+    <main className="h-screen flex flex-col items-center justify-center gap-8">
+      <img src={logo} alt="in.orbit" />
+
+      <img
+        src={rocketLaunchIllustration}
+        alt="Ilustração de uma mulher controlando um lançamento de um foguete através de um controle remoto"
+      />
 
       <p className="text-zinc-300 leading-relaxed max-w-80 text-center">
-        Você ainda não cadastrou nenhuma meta, que tal cadastrar um agora mesmo?
+        Você ainda não cadastrou nenhuma meta, que tal cadastrar uma agora
+        mesmo?
       </p>
 
       <DialogTrigger asChild>
         <Button>
-          <PlusIcon size={16} />
+          <Plus className="size-4" />
           Cadastrar meta
         </Button>
       </DialogTrigger>
-    </div>
+    </main>
   )
 }
